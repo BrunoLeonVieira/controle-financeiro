ba0dd12e1ab8d17a6d8a82a79be55a4580957469
Bruno Cruz <brunolvcruz@gmail.com>
2020-09-09 22:24:44 -0300

    Implementaçao CRUD para transactions

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,75 @@
-const express = require('express');
+const express = require("express");
 const transactionRouter = express.Router();
+const TransactionModel = require("../models/TransactionModel");
+const {
+  getTransactions,
+  createTransaction,
+  deleteTransaction,
+  alterTransaction,
+} = require("../services/transactionService");
+
+//BUSCA TRANSAÇÕES COM BASE EM ANO E MES INFORMADOS POR PARAMETRO NO FORMATO yyyy-MM
+transactionRouter.get("/:period", async (req, res, next) => {
+  try {
+    const periodo = req.params.period;
+    if (!periodo)
+      throw new Error(
+        "Erro - Necessário informar periodo de busca no formado yyyy-MM"
+      );
+
+    const data = await getTransactions(periodo);
+    res.send(JSON.stringify(data));
+  } catch (error) {
+    next(error);
+  }
+});
+
+//CRIA UMA NOVA TRANSACTION COM BASE NO JSON ENVIADO EM BODY
+transactionRouter.post("/", async (req, res, next) => {
+  try {
+    const transction = await createTransaction(req.body);
+    res.send(JSON.stringify({ transction }));
+  } catch (error) {
+    next(error);
+  }
+});
+
+//ALTERA DADOS DE UMA TRANSACTION
+transactionRouter.put("/:id", async (req, res, next) => {
+  try {
+    const transaction = req.body;
+    const id = req.params.id;
+
+    if (!id) throw new Error("Erro - Informe o id");
+
+    if (!transaction) throw new Error("Erro - Informe os dados de atualização");
+
+    const ret = await alterTransaction(id, transaction);
+
+    res.send(JSON.stringify(ret));
+  } catch (error) {
+    next(error);
+  }
+});
+
+//DELETA UMA TRANSACTION
+transactionRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+
+    if (!id) throw new Error("Erro - Necessário informar o id");
+
+    const aux = await deleteTransaction(id);
+    await res.end();
+  } catch (error) {
+    next(error);
+  }
+});
+
+//TRATAMENTO DE ERRO GENERICO
+transactionRouter.use((err, req, res, next) => {
+  console.log(`${req.method} ${req.baseUrl} - ${err.message}`);
+  res.status(400).send({ error: err.message });
+});
 
 module.exports = transactionRouter;
--- services/transactionService.js
+++ services/transactionService.js
@@ -1,8 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
 
 // Aqui havia um erro difícil de pegar. Importei como "transactionModel",
 // com "t" minúsculo. No Windows, isso não faz diferença. Mas como no Heroku
 // o servidor é Linux, isso faz diferença. Gastei umas boas horas tentando
 // descobrir esse erro :-/
-const TransactionModel = require('../models/TransactionModel');
+const TransactionModel = require("../models/TransactionModel");
+
+async function getTransactions(period) {
+  try {
+    const condition = { yearMonth: period };
+    return await TransactionModel.find(condition);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createTransaction(transaction) {
+  try {
+    const data = new TransactionModel(transaction);
+    return await data.save();
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function deleteTransaction(id) {
+  try {
+    return await TransactionModel.findByIdAndDelete(id);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function alterTransaction(id, transaction) {
+  try {
+    return await TransactionModel.findByIdAndUpdate(id, transaction);
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  getTransactions,
+  createTransaction,
+  deleteTransaction,
+  alterTransaction,
+};
